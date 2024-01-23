8adb8571977e20433b6a19296ac96eada16c641f
briksdaal <102250834+briksdaal@users.noreply.github.com>
2024-01-24 01:37:09 +0200

    Add return to skips to fix stalling in production

--- routes/middleware/skips.js
+++ routes/middleware/skips.js
@@ -2,21 +2,21 @@ const asyncHandler = require('express-async-handler');
 
 exports.skipIfLoggedIn = asyncHandler((req, res, next) => {
   if (req.isAuthenticated()) {
-    res.redirect('/');
+    return res.redirect('/');
   }
   next();
 });
 
 exports.skipIfVerified = asyncHandler((req, res, next) => {
   if (req.user.membershipStatus === 'Verified') {
-    res.redirect('/');
+    return res.redirect('/');
   }
   next();
 });
 
 exports.skipIfAdmin = asyncHandler((req, res, next) => {
   if (req.user.admin) {
-    res.redirect('/');
+    return res.redirect('/');
   }
   next();
 });
